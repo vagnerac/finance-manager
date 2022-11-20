7e22620c404f5f99482e86b7db73e0dcb231dc0a
Vagner Coelho <vagner.amorim@gmail.com>
2022-11-20 11:53:28 -0300

    package.json fixed

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,3 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const port = process.env.PORT;
-_app2.default.listen(port);
+_app2.default.listen(process.env.PORT);
