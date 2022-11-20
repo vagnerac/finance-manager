4990e6641b3959f64c64ca4a0034933503eba9af
Vagner Coelho <vagner.amorim@gmail.com>
2022-11-19 21:07:23 -0300

    Try block added to the token controller

--- dist/controllers/TokenController.js
+++ dist/controllers/TokenController.js
@@ -3,34 +3,38 @@ var _User = require('../models/User'); var _User2 = _interopRequireDefault(_User
 
 class TokenController {
   async store(req, res) {
-    const { email = '', password = '' } = req.body;
+    try {
+      const { email = '', password = '' } = req.body;
 
-    if (!email || !password) {
-      return res.status(401).json({
-        errors: ['Invalid credentials.'],
+      if (!email || !password) {
+        return res.status(401).json({
+          errors: ['Invalid credentials.'],
+        });
+      }
+
+      const user = await _User2.default.findOne({ where: { email } });
+
+      if (!user) {
+        return res.status(401).json({
+          errors: ['User not identified.'],
+        });
+      }
+
+      if (!(await user.passwodIsValid(password))) {
+        return res.status(401).json({
+          errors: ['Invalid password.'],
+        });
+      }
+
+      const { id } = user;
+      const token = _jsonwebtoken2.default.sign({ id, email }, process.env.TOKEN_SECRET, {
+        expiresIn: process.env.TOKEN_EXPIRATION,
       });
+      return res.json({ token });
+    } catch (e) {
+      console.log(e);
+      return res.json();
     }
-
-    const user = await _User2.default.findOne({ where: { email } });
-
-    if (!user) {
-      return res.status(401).json({
-        errors: ['User not identified.'],
-      });
-    }
-
-    if (!(await user.passwodIsValid(password))) {
-      return res.status(401).json({
-        errors: ['Invalid password.'],
-      });
-    }
-
-    const { id } = user;
-    const token = _jsonwebtoken2.default.sign({ id, email }, process.env.TOKEN_SECRET, {
-      expiresIn: process.env.TOKEN_EXPIRATION,
-    });
-
-    return res.json({ token });
   }
 }
 
--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,4 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const port = 3000;
-_app2.default.listen(port, () => {
-  console.log();
-  console.log(`Escutando a porta ${port}`);
-  console.log(`CTRL + clique em http://localhost:${port}`);
-});
+const port = process.env.PORT;
+_app2.default.listen(port);
