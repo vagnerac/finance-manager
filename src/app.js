b36300528588de5b4a1b22e9b6cd31b3b98afe53
Vagner Coelho <vagner.amorim@gmail.com>
2022-12-08 23:02:41 -0300

    wikipedia added to the allowed address

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ import './database';
 const whiteList = [
   'https://react.vagnercoelho.dev',
   'http://localhost:3000',
-  'https://pt.wikipedia.org/',
+  'https://pt.wikipedia.org',
 ];
 
 const corsOptions = {
